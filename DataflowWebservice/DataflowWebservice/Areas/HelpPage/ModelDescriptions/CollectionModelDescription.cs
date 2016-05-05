5d5319aff10c08e502b7c250f6c213412fdf1a57
swenmeeuwes <swenmeeu@gmail.com>
2016-05-05 16:29:07 +0200

    Added: Empty Webservice with a HTTP Request example project

---
+++ DataflowWebservice/DataflowWebservice/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DataflowWebservice.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
