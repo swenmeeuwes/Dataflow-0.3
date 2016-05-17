0688c0ec91ea97ed5e45fb4cd17c4e11ae8dc713
Swen <swenmeeu@gmail.com>
2016-05-17 10:41:00 +0200

    Added: Entities + Enemies + Walk to point

---
+++ MilitaireSimulatie/Assets/Resources/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Entity
+{
+    public float speed;
+
+    [SerializeField]
+    Vector3 finishPoint;
+    [SerializeField]
+    float allowedDistance;
+
+    Vector3 direction;
+
+    public void MoveTo(Vector3 finishPoint)
+    {
+        direction = (finishPoint - transform.position).normalized * speed;
+
+        StartCoroutine(SmoothMovement(finishPoint));
+    }
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+
+        MoveTo(finishPoint);
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    IEnumerator SmoothMovement(Vector3 destination)
+    {
+        var remainingDistance = (destination - transform.position).magnitude;
+        while (remainingDistance > allowedDistance)
+        {
+            rbody.velocity = new Vector3(direction.x * Time.deltaTime, rbody.velocity.y, direction.z * Time.deltaTime);
+            remainingDistance = (destination - transform.position).magnitude;
+            yield return null;
+        }
+    }
+}
---
+++ MilitaireSimulatie/Assets/Resources/Scripts/Entity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public abstract class Entity : MonoBehaviour {
+    protected Rigidbody rbody;
+    protected CapsuleCollider capsuleCollider;
+
+    protected int health = 100;
+
+    protected virtual void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    protected virtual void Update() {
+        // if hit remove health
+    }
+    protected virtual void OnValidate() { }
+}
